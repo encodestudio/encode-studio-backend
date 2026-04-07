d1ee498d293d362b0cedfb10c9a91b9a13a90548
Shivam <encodestudio.in@gmail.com>
2026-04-07 18:14:46 +0530

    debugging dashboard login credentials

--- pages/api/admin-login.js
+++ pages/api/admin-login.js
@@ -41,6 +41,9 @@ export default async function handler(req, res) {
         // 🔍 Optional Debug (remove after testing)
         // console.log("INPUT:", inputEmail, inputPassword);
         // console.log("ENV:", adminEmail, adminPassword);
+        console.log("ENV EMAIL:", process.env.ADMIN_EMAIL);
+        console.log("ENV PASSWORD:", process.env.ADMIN_PASSWORD);
+        console.log("BODY:", req.body);
 
         // ✅ Credential Check
         if (inputEmail === adminEmail && inputPassword === adminPassword) {
